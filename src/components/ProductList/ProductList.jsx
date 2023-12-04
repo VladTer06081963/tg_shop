6bf88fe099d5560931562a92521734e6149a79e4
VladTer06081963 <patrol200611@ukr.net>
2023-12-04 11:00:26 +0200

    add new list

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -5,14 +5,14 @@ import {useTelegram} from "../../hooks/useTelegram";
 import {useCallback, useEffect} from "react";
 
 const products = [
-    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '1', title: 'Пельмени', price: 500, description: 'говядина/свинина Bolinhos 50/50 - carne bovina/porca'},
+    {id: '2', title: 'Вареники с картофелем', price: 120, description: 'Varéniki com batata'},
+    {id: '3', title: 'Блинчики с мясом', price: 50, description: 'Panquecas com carne'},
+    {id: '4', title: 'Котлеты', price: 122, description: ' говядина Costeletas de carne bovina'},
+    {id: '5', title: 'Паштеты', price: 50, description: 'Patê'},
+    {id: '6', title: 'Сырники', price: 60, description: '(печеные в духовке) Sirniki (assado no forno)'},
+    {id: '7', title: 'Мацони', price: 550, description: 'Matsoni'},
+    {id: '8', title: 'Штрудель', price: 120, description: 'Strudel'},
 ]
 
 const getTotalPrice = (items = []) => {
